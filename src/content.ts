6dbd3ac8757d4861118e6eb5d1d427bc32e3bf65
Tommy May <tommymay37@gmail.com>
2022-12-27 14:34:03 -0500

    fix(src/content.ts): changing URLs witout refreshing was not generating the link, now it does
    Twitch is an SPA, so when the user navigates to another page our extension was not reacting to the
    change. We are now observing URL changes and reacting.

--- src/content.ts
+++ src/content.ts
@@ -4,17 +4,64 @@ try {
   console.error(error)
 }
 
+const cache: Partial<Record<string, string>> = {}
+
 async function main (): Promise<void> {
-  const twitchClipEl = await waitForEl<HTMLVideoElement>('video', (el) => isTwitchClip(el.src))
-  return await makeTitleDownloadableLink(twitchClipEl.src)
+  onUrlChange(function handleUrlChange (url, oldUrl) {
+    const isClipPage = /^https:\/\/www.twitch.tv\/.*\/clip\/.*/.test(url)
+    if (!isClipPage) return
+
+    const cachedValue = cache[url]
+    if (cachedValue !== undefined) {
+      makeTitleDownloadableLink(cachedValue, url)
+        .catch(console.error)
+      return
+    }
+
+    waitForEl<HTMLVideoElement>('video', (videoEl) => {
+      // Best way to explain this is the page URL will change, but that doesn't mean the clip on the page has
+      // changed yet since Twitch is an SPA. This is why we need to check if the clip we found is from the old URL.
+      const isOldPage = cache[oldUrl] === videoEl.src
+
+      return !isOldPage && isTwitchClip(videoEl.src)
+    })
+      .then(async videoEl => {
+        cache[url] = videoEl.src
+        await makeTitleDownloadableLink(videoEl.src, url)
+      })
+      .catch(console.error)
+  })
+}
+
+let currentUrl: string
+/**
+ * This is a bit hacky of a solution, but essentially anytime the document changes we check if the URL has changed.
+ * There isn't a great way to listen to URL changes, but unfortunately events like "popstate" do not work as expected.
+ * i.e. The "popstate" event is only fired during browser events like the forward and back button but not when
+ * history.pushState is called and Twitch is an SPA, so they use history.pushState for routing. This event is fired
+ * on the initial URL as well as subsequent URL changes.
+ * @param callback
+ */
+function onUrlChange (callback: (url: string, oldUrl: string) => void): void {
+  const observer = new MutationObserver(mutations => {
+    if (currentUrl === window.location.href) return
+    const oldUrl = currentUrl
+    currentUrl = window.location.href
+    callback(currentUrl, oldUrl)
+  })
+  observer.observe(window.document, {
+    childList: true,
+    subtree: true
+  })
 }
 
 function isTwitchClip (link: string): boolean {
   return link.startsWith('https://production.assets.clips.twitchcdn.net')
 }
 
-async function makeTitleDownloadableLink (link: string): Promise<void> {
+async function makeTitleDownloadableLink (link: string, forUrl: string): Promise<void> {
   const titleEl = await waitForEl('[data-a-target="stream-title"]')
+  if (window.location.href !== forUrl) return
   titleEl.innerHTML = `<a download href='${link}'>${titleEl.textContent ?? 'Untitled Clip'}</a>`
 }
 
@@ -35,8 +82,6 @@ async function waitForEl<E extends Element> (selector: string, condition?: Condi
       }
     })
 
-    // eslint-disable-next-line prefer-const
-    let timeoutId: ReturnType<typeof setTimeout> | undefined
     const observer = new MutationObserver(() => {
       const els = document.querySelectorAll<E>(selector)
       let el: E | undefined
@@ -49,10 +94,6 @@ async function waitForEl<E extends Element> (selector: string, condition?: Condi
       if (el === undefined) return
 
       if (condition === undefined || condition(el)) {
-        if (timeoutId !== undefined) {
-          clearTimeout(timeoutId)
-        }
-
         observer.disconnect()
         return resolve(el)
       }
@@ -62,12 +103,6 @@ async function waitForEl<E extends Element> (selector: string, condition?: Condi
       childList: true,
       subtree: true
     })
-
-    // We should eventually give up on listening, 3 minutes is arbitrary, but feels like enough time to stop.
-    timeoutId = setTimeout(function stopListening () {
-      observer.disconnect()
-      reject(new Error(`Timeout waiting for selector ${selector}`))
-    }, 180000)
   })
 }
 
